991a1601ca4b1405441061c2af1574c6fbb1c109
Alexander <alexander.neitzel@codineerdigital.com>
2022-12-30 19:13:50 +0100

    Wrote a test

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,3 +1,58 @@
-test('test the functionality of the test setup', () => {
-    console.log('test');
+import { DocumentLockManager } from '..';
+
+describe('DocumentLockManager', () => {
+  let lockManager: DocumentLockManager;
+
+  beforeEach(() => {
+    lockManager = new DocumentLockManager();
+  });
+
+  it('should execute operations in the correct order', async () => {
+    const result: number[] = [];
+
+    lockManager.lock('doc1', async () => {
+      result.push(1);
+      await delay(10);
+      result.push(2);
+    });
+
+    lockManager.lock('doc1', async () => {
+      result.push(3);
+      await delay(10);
+      result.push(4);
+    });
+
+    lockManager.lock('doc1', async () => {
+      result.push(5);
+      await delay(10);
+      result.push(6);
+    });
+
+    await delay(100);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it('should execute operations for different documents concurrently', async () => {
+    const result: number[] = [];
+
+    lockManager.lock('doc1', async () => {
+      result.push(1);
+      await delay(50);
+      result.push(2);
+    });
+
+    lockManager.lock('doc2', async () => {
+      result.push(3);
+      await delay(10);
+      result.push(4);
+    });
+
+    await delay(100);
+    expect(result).toEqual([1, 3, 4, 2]);
+  });
+
+  function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 });
+
